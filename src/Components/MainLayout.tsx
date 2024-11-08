bcce1da96a57b2fd8d817972eb761372d95951e0
Abd Eltawab Shaaban <scob198350@gmail.com>
2024-11-08 23:17:26 +0200

    Add Footer

--- src/Components/MainLayout.tsx
+++ src/Components/MainLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./Nav/NavBar";
+import Footer from "./Footer/Footer";
 
 export default function MainLayout() {
     return (
         <div className="w-full min-h-fit">
             <NavBar />
             <Outlet />
+            <Footer/>
         </div>
     )
 }
