deb00d864726fcd348ae9606efe37a59526a68c3
Abd Eltawab Shaaban <scob198350@gmail.com>
2024-11-10 20:42:42 +0200

    Fixed some error

--- src/Page/SingleProduct/ProductPricing.tsx
+++ src/Page/SingleProduct/ProductPricing.tsx
@@ -10,7 +10,7 @@ const ProductPricing: React.FC<ProductPricingProps> = ({ price, oldPrice }) => {
         <div className="flex flex-wrap gap-4 mt-8">
             <p className="text-white text-4xl font-semibold">${price}</p>
             <p className="text-gray-400 text-base">
-                <strike>${oldPrice}</strike>
+                <span className="text-white line-through">${oldPrice}</span>
                 <span className="text-sm ml-1">Tax included</span>
             </p>
         </div>
